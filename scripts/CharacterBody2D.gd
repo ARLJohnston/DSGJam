c7f426bf6982a7865399e35ef35b23dbd51e84b0
Karl Hartmann <2566374h@student.gla.ac.uk>
2024-02-23 21:15:37 +0000

    added accelleration

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
+@export var decel = 0.9
 @export var speed = 400
+var input_direction = 0
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	input_direction = Input.get_vector("left", "right", "up", "down")
 
 func _physics_process(delta):
 	get_input()
+	if input_direction[0] != 0 or input_direction[1] != 0:
+		velocity = input_direction * speed
+	else:
+		velocity = velocity * decel
 	move_and_slide()
