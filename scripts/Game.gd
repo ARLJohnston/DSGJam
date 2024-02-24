7b1aa20687b8b2aff7ba9d251b45485ca51ce0e7
Dan <knifacat@gmail.com>
2024-02-24 17:36:31 +0000

    wip

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -6,56 +6,68 @@ extends Node2D
 @onready var plant_scene = preload("res://scenes/Plant.tscn")
 
 
-const BASE_SIZE = Vector2(32, 16)
+const BASE_SIZE = Vector2i(32, 16)
 const TILE_SIZE = 64
 
 var items = ItemProtoset
 var plants: Array[Plant] = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	# Shift player to the center of the world
-	player.position = Vector2(BASE_SIZE.x * TILE_SIZE / 2, BASE_SIZE.y * TILE_SIZE / 2)
+	player.position = _tilemap_to_position(Vector2i(BASE_SIZE.x / 2, BASE_SIZE.y / 2))
+	_gen()
 
-	_gen_plants()
-	_gen_world()
+func _gen():
+	for i in range(5):
+		var new_plant = plant_scene.instantiate()
 
-func _gen_plants():
-	for i in range(0, 5):
-		var plant = plant_scene.instantiate()
+		if plants.size() == 0:
+			new_plant.position = _tilemap_to_position(_random_tilemap_position())
+		else:
+			var candidate_position = _position_to_tilemap(plants[0].position)
+			while true:
+				candidate_position = _random_tilemap_position()
 
-		plant.position = Vector2(randi_range(0, BASE_SIZE.x - 1) * TILE_SIZE, randi_range(0, BASE_SIZE.y - 1) * TILE_SIZE)
-		plant.position += Vector2(TILE_SIZE / 2, TILE_SIZE / 2)
+				var is_good = true
+				for plant in plants:
+					var distance = (_position_to_tilemap(plant.position) - candidate_position).length()
+					if distance < 5:
+						is_good = false
+						break
 
-		plant.get_node("Area2D").connect("body_entered", _on_plant_overlapped.bind(plant))
-
-		add_child(plant)
-		plants.append(plant)
-
-func _gen_world():
-	const threshold_to_tiles = {
-		0.5: 0,
-		0.6: 1,
-		0.65: 2,
-		0.8: 3,
-		1.0: 4,
-	}
-
-	var perlin_noise = FastNoiseLite.new()
-	perlin_noise.seed = randi()
-
-	for x in (BASE_SIZE.x):
-		for y in (BASE_SIZE.y):
-			var noise_at_point = (perlin_noise.get_noise_2d(x, y) + 1.0) / 2.0
-
-			var tile = 0
-			for threshold in threshold_to_tiles.keys():
-				if noise_at_point < threshold:
-					tile = threshold_to_tiles[threshold]
+				if is_good:
+					new_plant.position = _tilemap_to_position(candidate_position)
 					break
 
 
-			tilemap.set_cell(0, Vector2(x, y), 2, Vector2(tile, 0))
+		new_plant.get_node("Area2D").connect("body_entered", _on_plant_overlapped.bind(new_plant))
+
+		add_child(new_plant)
+		plants.append(new_plant)
+
+	# const threshold_to_tiles = {
+	# 	0.5: 0,
+	# 	0.6: 1,
+	# 	0.65: 2,
+	# 	0.8: 3,
+	# 	1.0: 4,
+	# }
+
+	# var perlin_noise = FastNoiseLite.new()
+	# perlin_noise.seed = randi()
+
+	# for x in (BASE_SIZE.x):
+	# 	for y in (BASE_SIZE.y):
+	# 		var noise_at_point = (perlin_noise.get_noise_2d(x, y) + 1.0) / 2.0
+
+	# 		var tile = 0
+	# 		for threshold in threshold_to_tiles.keys():
+	# 			if noise_at_point < threshold:
+	# 				tile = threshold_to_tiles[threshold]
+	# 				break
+
+
+	# 		tilemap.set_cell(0, Vector2(x, y), 2, Vector2(tile, 0))
 
 func _on_plant_overlapped(_body, plant):
 	var label = $CanvasLayer/Label
@@ -67,6 +79,11 @@ func _on_plant_overlapped(_body, plant):
 
 	label.text = text
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _position_to_tilemap(pos: Vector2) -> Vector2i:
+	return Vector2i(int(pos.x / TILE_SIZE), int(pos.y / TILE_SIZE))
+
+func _tilemap_to_position(tile: Vector2i) -> Vector2:
+	return Vector2(tile.x * TILE_SIZE, tile.y * TILE_SIZE) + Vector2(TILE_SIZE / 2, TILE_SIZE / 2)
+
+func _random_tilemap_position() -> Vector2i:
+	return Vector2i(randi() % BASE_SIZE.x, randi() % BASE_SIZE.y)
