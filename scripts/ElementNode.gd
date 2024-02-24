ac2fe1f02a5dffc692396ceee78cb4107f7b8ce4
GoldenDragonXIV <anonymous@example.com>
2024-02-24 13:51:09 +0000

    ElementNode

--- scripts/ElementNode.gd
+++ scripts/ElementNode.gd
@@ -1,14 +1,26 @@
-extends Node
+extends Node2D
 
+var value = 1
+var last_animation
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Label.text = value
 
 func remove():
-	pass
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	value -= 1
+	$Label.text = str(value)
+	if value <= 0:
+		$AnimationPlayer.play("die")
+	else:
+		$AnimationPlayer.play("modify_down")
+
+func add():
+	value += 1
+	$Label.text = str(value)
+	$AnimationPlayer.play("modify_up")
+
+func _on_animation_player_animation_finished(anim_name):
+	last_animation = anim_name
+	if(anim_name == "die"):
+		queue_free()
