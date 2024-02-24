9aa28031d95a3872b089741d7bb5dcc4a8974088
GoldenDragonXIV <anonymous@example.com>
2024-02-24 12:46:52 +0000

    More folders

---
+++ scripts/cauldron.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var resolver
+var contents = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	resolver = ElementResolver.new()
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+_on_
