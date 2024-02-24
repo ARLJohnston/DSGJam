b68b690e02410f5ef17723f1f36d8f08b9fc1b98
GoldenDragonXIV <anonymous@example.com>
2024-02-24 20:29:15 +0000

    plant colors

--- scripts/Plant.gd
+++ scripts/Plant.gd
@@ -1,35 +1,44 @@
 class_name Plant
 extends Node2D
 
-const plant_types = [
-	"air",
-	"earth",
-	"fire",
-	"water",
-]
+var plant_types
 
 @export var plant_stats = {}
 
 @export var dominant_plant_type = "air"
+@export var petal_rotation = 0
 
 @onready var sprite = $Sprite2D
 
 func _ready():
+	plant_types = $ElementResolver.elements_definition.keys()
+	plant_types.erase("null")
+	
 	dominant_plant_type = plant_types[randi() % plant_types.size()]
 	plant_stats[dominant_plant_type] = randi() % 3 + 1
+	petal_rotation = 360/(plant_stats[dominant_plant_type])
 
 	var has_secondary_type = randf() < 0.5
+	var secondary_type = dominant_plant_type
 	if has_secondary_type:
-		var secondary_type = dominant_plant_type
 		while secondary_type == dominant_plant_type:
 			secondary_type = plant_types[randi() % plant_types.size()]
-
-		plant_stats[secondary_type] = randi() % 1 + 1
+			
+		plant_stats[secondary_type] = randi() % plant_stats[dominant_plant_type] + 1
+		petal_rotation = 360/(plant_stats[dominant_plant_type]*plant_stats[secondary_type]) % 360
 	
 	$ProximityElementDisplay.set_data(plant_stats)
-
-	sprite.texture.gradient = sprite.texture.gradient.duplicate()
-	sprite.texture.gradient.set_color(0, plant_type_to_color[dominant_plant_type])
+	
+	$flower_base.self_modulate = Color($ElementResolver.elements_definition[dominant_plant_type].color)
+	var image_resource = "assets/flower_base_" + str([1,2,3].pick_random()) + ".png"
+	$flower_base.texture = load(image_resource)
+	
+	$flower_petals.self_modulate = Color($ElementResolver.elements_definition[secondary_type].color)
+	$flower_petals.rotation_degrees = petal_rotation
+	$flower_petals.rotate(rotation_degrees)
+	
+	image_resource = "assets/flower_petals_" + str([1,2,3].pick_random()) + ".png"
+	$flower_petals.texture = load(image_resource)
 
 func _process(delta):
 	pass
--- scripts/element.gd
+++ scripts/element.gd
@@ -3,8 +3,10 @@ class_name Element
 var name: String
 var created_by
 var resource
+var color
 	
 func _init(name: String, created_by = [], color="#FFFFFF", resource = "res://assets/elements/"+name+".png"):
 	self.name = name
 	self.created_by = created_by
+	self.color = color
 	self.resource = resource
