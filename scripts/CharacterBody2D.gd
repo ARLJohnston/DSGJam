36aa26513030f8e6e3bc4c52e3b73b515d1ab3e9
Karl Hartmann <2566374h@student.gla.ac.uk>
2024-02-24 18:08:13 +0000

    Removed diagonal movement

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 @export var speed = 500
+var prev_direction = Vector2(0, 0)
 var input_direction = Vector2(0, 0)
 var jumping = 0
 var new_position = Vector2(0, 0)
@@ -12,9 +13,15 @@ func _ready():
 	new_position = position
 
 func get_input():
+	prev_direction = input_direction
 	input_direction = round(Input.get_vector("left", "right", "up", "down"))
+	if input_direction[0] != 0 and input_direction[1] != 0:
+		if prev_direction[0] == 0 or prev_direction[1] == 0:
+			input_direction = prev_direction
+		else:
+			input_direction = Vector2(0, 0)
 
-func move(dir):
+func move(dir):		
 	if !tween or !tween.is_running():
 		#move by 64*direction
 		tween = create_tween()
