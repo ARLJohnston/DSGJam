969014ef2d4dffb3a7943a873bb1f5ef813a1c66
GoldenDragonXIV <anonymous@example.com>
2024-02-25 17:13:50 +0000

    Fix grandma

--- scripts/Grandma.gd
+++ scripts/Grandma.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var request = {}
-var min_timer_amount = 60
+var min_timer_amount = 260
 var task_complete = false
 
 # Called when the node enters the scene tree for the first time.
@@ -15,20 +15,27 @@ func _ready():
 		$ProximityElementDisplay.set_data(request)
 
 func make_request(map):
-	
 	var plants = map.plants
 	if plants.size() == 0:
 		return
 		
-	var indexes = [0,0,0]
-	var unique = false
-	while !unique:
-		indexes = [randi()%plants.size(), randi()%plants.size(), randi()%plants.size()]
-		# big brain
-		if indexes[0] != indexes[1] && indexes[1] != indexes[2] && indexes[0] != indexes[2]:
-			unique = true
+	request = {}
+	
+	var new_plants
+	
+	if plants.size() < 3:
+		new_plants = plants.duplicate()
+	else:	
+		var indexes = [0,0,0]
+		var unique = false
+		while !unique:
+		
+			indexes = [randi()%plants.size(), randi()%plants.size(), randi()%plants.size()]
+			# big brain
+			if indexes[0] != indexes[1] && indexes[1] != indexes[2] && indexes[0] != indexes[2]:
+				unique = true
 			
-	var new_plants = [plants[indexes[0]], plants[indexes[1]], plants[indexes[2]]]
+		new_plants = [plants[indexes[0]], plants[indexes[1]], plants[indexes[2]]]
 	
 	for plant in new_plants:
 		request = $ElementResolver.merge_elements(request, plant.data.plant_stats)
